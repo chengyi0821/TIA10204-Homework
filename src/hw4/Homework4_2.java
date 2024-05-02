9e5097d4b8e63295864a3ed209ce7aec611595f2
Cheng Yi Lin <charlielin05060506@gmail.com>
2024-05-02 16:42:31 +0800

    Homework update

--- src/hw4/Homework4_2.java
+++ src/hw4/Homework4_2.java
@@ -3,7 +3,17 @@ package hw4;
 public class Homework4_2 {
 	public static void main(String[] args) {
 		String s = "Hello World";
-		StringBuilder q = new StringBuilder(s).reverse();
-        System.out.println(q);
+		String[] strArray = s.split("");
+		for(int i = strArray.length - 1 ; i >= 0 ; i--) {
+			System.out.print(strArray[i]);
+		}
+//		char[] charArray = s.toCharArray();
+//		StringBuilder q = new StringBuilder(s).reverse();
+//        System.out.println(q);
+//		for(int i = charArray.length; i > 0 ; i--) {
+//			System.out.print(charArray[i - 1]);
+//		}
+//		System.out.println(s[0].length());
+		
     }
 }
