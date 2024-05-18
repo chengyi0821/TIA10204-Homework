edaa6447924a22203e85ec95cd6c818a416c1f34
Cheng Yi Lin <charlielin05060506@gmail.com>
2024-05-18 21:20:02 +0800

    HomeWork8 Update

--- src/hw8/HomeWork8_1.java
+++ src/hw8/HomeWork8_1.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class HomeWork8_1 {
 	public static void main(String[] args) {
 		List<Object> data = new ArrayList<>();
+		
 		data.add(new Integer(100));
 		data.add(new Double(3.14));
 		data.add(new Long(21L));
@@ -18,19 +19,21 @@ public class HomeWork8_1 {
 		data.add(new Object());
 		data.add("Snoppy");
 		data.add(new BigInteger("1000"));
+		
 		System.out.println("======================集合物件的所有元素====================");
 		for(Object obj : data)
 			System.out.print(obj + " ");
+		
 		System.out.println("\n======================foreach====================");
-		for(Object obj : data) {
+		for(Object obj : data) 
 			if(!(obj instanceof Number))
 			System.out.print(obj + " ");
-		}
+		
 		System.out.println("\n======================for迴圈====================");
-		for(int i = 0;i < data.size() ; i++) {
+		for(int i = 0;i < data.size() ; i++) 
 			if(!(data.get(i) instanceof Number))
 			System.out.print(data.get(i) + " ");
-		}
+		
 		System.out.println("\n======================Iterator====================");
 		Iterator iterator = data.iterator();
 		while(iterator.hasNext()) {
--- src/hw8/HomeWork8_2.java
+++ src/hw8/HomeWork8_2.java
@@ -6,6 +6,7 @@ import java.util.TreeSet;
 public class HomeWork8_2 {
 	public static void main(String[] args) {
 		TreeSet<Train> set = new TreeSet<Train>();
+		
 		Train t1 = new Train(202, "普悠瑪", "樹林", "花蓮", 400);
 		Train t2 = new Train(1254, "區間", "屏東", "基隆", 700);
 		Train t3 = new Train(118, "自強", "高雄", "台北", 500);
@@ -13,7 +14,7 @@ public class HomeWork8_2 {
 		Train t5 = new Train(122, "自強", "台中", "花蓮", 600);
 		Train t6 = new Train(1222, "區間", "樹林", "七堵", 300);
 		Train t7 = new Train(1254, "區間", "屏東", "基隆", 700);
-
+		
 		set.add(t1);
 		set.add(t2);
 		set.add(t3);
@@ -21,16 +22,19 @@ public class HomeWork8_2 {
 		set.add(t5);
 		set.add(t6);
 		set.add(t7);
+		
 		System.out.println("=================foreach=================");
 		for (Train train : set) {
 			train.getAll();
 		}
+		
 		System.out.println("=================迭代器=================");
 		Iterator<Train> it= set.iterator();
 		while(it.hasNext()) {
 			Train train = it.next();
 			train.getAll();
 			} 
+		
 		System.out.println("=================for迴圈=================");
 		Train[] tArray = set.toArray(new Train[0]); //將TreeSet轉成Array
         for (int i = 0; i < tArray.length; i++) {
