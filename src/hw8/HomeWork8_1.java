f8a30238ccaad77dd4dbe5b6234ca5b7b82d6026
Cheng Yi Lin <charlielin05060506@gmail.com>
2024-05-18 19:55:44 +0800

    HomeWork8

---
+++ src/hw8/HomeWork8_1.java
@@ -0,0 +1,42 @@
+package hw8;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class HomeWork8_1 {
+	public static void main(String[] args) {
+		List<Object> data = new ArrayList<>();
+		data.add(new Integer(100));
+		data.add(new Double(3.14));
+		data.add(new Long(21L));
+		data.add(new Short("100"));
+		data.add(new Double(5.1));
+		data.add("Kitty");
+		data.add(new Integer(100));
+		data.add(new Object());
+		data.add("Snoppy");
+		data.add(new BigInteger("1000"));
+		System.out.println("======================集合物件的所有元素====================");
+		for(Object obj : data)
+			System.out.print(obj + " ");
+		System.out.println("\n======================foreach====================");
+		for(Object obj : data) {
+			if(!(obj instanceof Number))
+			System.out.print(obj + " ");
+		}
+		System.out.println("\n======================for迴圈====================");
+		for(int i = 0;i < data.size() ; i++) {
+			if(!(data.get(i) instanceof Number))
+			System.out.print(data.get(i) + " ");
+		}
+		System.out.println("\n======================Iterator====================");
+		Iterator iterator = data.iterator();
+		while(iterator.hasNext()) {
+				Object obj = iterator.next();
+				if(!(obj instanceof Number))
+					System.out.print(obj+" ");
+		}
+	}
+}
---
+++ src/hw8/Train.java
@@ -0,0 +1,96 @@
+package hw8;
+
+import java.util.Objects;
+
+public class Train implements Comparable<Train> {
+	private int number; // 班次
+	private String type; // 車種
+	private String start; // 出發地
+	private String dest; // 目的地
+	private double price; // 票價
+
+	public Train() {
+	};
+
+	public Train(int number, String type, String start, String dest, double price) {
+		this.number = number;
+		this.type = type;
+		this.start = start;
+		this.dest = dest;
+		this.price = price;
+	}
+
+	public void getAll() {
+		System.out.println(number + "," + type + "," + start + "," + dest + "," + price);
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+	public String getDest() {
+		return dest;
+	}
+
+	public void setDest(String dest) {
+		this.dest = dest;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dest, number, price, start, type);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Train other = (Train) obj;
+		return Objects.equals(dest, other.dest) && number == other.number
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price)
+				&& Objects.equals(start, other.start) && Objects.equals(type, other.type);
+	}
+
+	@Override
+	public int compareTo(Train o) {
+		if (this.number < o.number) {
+			return 1;
+		} else if (this.number == o.number) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+}
